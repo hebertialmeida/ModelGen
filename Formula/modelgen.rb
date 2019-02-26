0c02c9a50583b5d107d1d954952ccf36019493e9
Heberti Almeida <hebertialmeida@gmail.com>
2019-02-26 00:12:28 -0500

    Update modelgen.rb

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -1,7 +1,9 @@
 class Modelgen < Formula
   desc "Swift CLI to generate Models based on a JSON Schema and a template."
   homepage "https://github.com/hebertialmeida/ModelGen"
-  url "https://github.com/hebertialmeida/ModelGen.git", :tag => "0.4.0"
+  url "https://github.com/hebertialmeida/ModelGen.git", 
+    :tag => "0.4.0",
+    :revision => "5d4bb5ab8d8ba95098c8326ae1641e03e521ed3d"
   head "https://github.com/hebertialmeida/ModelGen.git"
 
   depends_on :xcode => ["10.1", :build]
