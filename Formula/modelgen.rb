c2f6677e249a02aa308f462c0052e83363082e09
Heberti Almeida <hebertialmeida@gmail.com>
2019-02-25 23:33:24 -0500

    Update modelgen.rb

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -3,7 +3,7 @@ class Modelgen < Formula
   homepage "https://github.com/hebertialmeida/ModelGen"
   url "https://github.com/hebertialmeida/ModelGen.git",
       :tag => "0.4.0",
-      :revision => "0c691f43f991778110525a27c072eebf907399b1"
+      :revision => "fe34b32d90dab18b1c6018a867d7c604e57d5e15"
   head "https://github.com/hebertialmeida/ModelGen.git"
 
   depends_on :xcode => ["10.1", :build]
