0c691f43f991778110525a27c072eebf907399b1
Heberti Almeida <hebertialmeida@gmail.com>
2019-02-25 23:08:33 -0500

    Update modelgen.rb

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -2,19 +2,17 @@ class Modelgen < Formula
   desc "Swift CLI to generate Models based on a JSON Schema and a template."
   homepage "https://github.com/hebertialmeida/ModelGen"
   url "https://github.com/hebertialmeida/ModelGen.git",
-      :tag => "0.3.0",
+      :tag => "0.4.0",
       :revision => "fc9b6c96294c08f95e266ee1bcb1463c515a8687"
   head "https://github.com/hebertialmeida/ModelGen.git"
 
-  depends_on :xcode => ["9.3", :build]
+  depends_on :xcode => ["10.1", :build]
 
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", "-static-stdlib"
-    bin.install ".build/release/modelgen"
-    lib.install Dir[".build/release/*.dylib"]
+    system "make", "install", "prefix=#{prefix}"
   end
 
   test do
-    system bin/"modelgen", "--version"
+    system "#{bin}/modelgen", "--version"
   end
 end
